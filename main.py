f841a0bcd84f3166960917fc2461c91489f629d9
Turner009 <009turner@gmail.com>
2022-12-04 21:24:26 -0600

    created functions for reverse_list and list_value_checker

---
+++ main.py
@@ -0,0 +1,11 @@
+# Make sure to import your problems here to test as you work!
+
+# Example (uncomment lines 4-8 to test):
+import problems_one
+
+to_reverse = [1, 2, 3, 4, 5, 6]
+x = 3
+print(problems_one.reverse_list(to_reverse))
+
+print(problems_one.list_value_checker(to_reverse,x))
+
